e69a77a4f637aa84a3901d8792501751840b3305
benhutchinson <ben.hutchinson.personal@gmail.com>
2014-11-23 20:06:37 +0000

    Added Some Auto-Validations To User Model (i.e. unique username, email) and 1-140 Character Count For Message Length In Post Model.  These are Datamapper functions.

--- app/user.rb
+++ app/user.rb
@@ -4,10 +4,10 @@ class User
 
   include DataMapper::Resource
 
-  property :id,   Serial
-  property :name, String
-  property :email, String
-  property :username, String
+  property :id,       Serial
+  property :name,     String
+  property :email,    String, :unique => true, :format => :email_address,
+  property :username, String, :unique => true,
   property :password, String
 
   has n, :posts
